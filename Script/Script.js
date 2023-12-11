bc5e5bd081a07a48b35dfa85a5a4a697e2e6b8be
Hardik <hardiik003@gmail.com>
2023-12-12 01:10:56 +0530

    Some New Changes

--- Script/Script.js
+++ Script/Script.js
@@ -1,38 +1,35 @@
-$(".carousel").owlCarousel(
-    {
-        margin: 10,
-        loop:true,
-        autoplay: true,
-        autoplayTimeout: 2000, // 1000ms is 1 sec... //
-        autoplayHoverPause: true,     
-        responsive: {
-           1200:{
-            items:1,
-            nav:false,
-           },         
-           1000:{
-            items:1,
-            nav:false,
-           },    
-           800:{
-            items:1,
-            nav:false,
-           },
-           500:{
-            items:1,
-            nav:false,
-           },
-           400:{
-            items:1,
-            nav:false,
-           },
-           200:{
-            items:1,
-            nav:false,
-           }       
-        }
-    }
-  );
-
+$(".carousel").owlCarousel({
+  margin: 10,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000, // 1000ms is 1 sec... //
+  autoplayHoverPause: true,
+  responsive: {
+    1200: {
+      items: 1,
+      nav: false,
+    },
+    1000: {
+      items: 1,
+      nav: false,
+    },
+    800: {
+      items: 1,
+      nav: false,
+    },
+    500: {
+      items: 1,
+      nav: false,
+    },
+    400: {
+      items: 1,
+      nav: false,
+    },
+    200: {
+      items: 1,
+      nav: false,
+    },
+  },
+});
 
 //  Btn Functions Written soon...
